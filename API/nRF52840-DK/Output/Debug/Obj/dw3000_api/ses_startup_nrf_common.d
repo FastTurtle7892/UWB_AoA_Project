bc8947a3739cb3aa1333548d939963de8afe1c84
YoonSeokapple <jangys7892@gmail.com>
2026-04-20 00:01:05 +0900

    refactoring

--- API/nRF52840-DK/Output/Debug/Obj/dw3000_api/ses_startup_nrf_common.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/dw3000_api/ses_startup_nrf_common.o: \
- C:\work_tree_2\AoA_Project\API\nRF52840-DK\sdk\modules\nrfx\mdk\ses_startup_nrf_common.s
--- API/nRF52840-DK/Output/Debug/Obj/dw3000_api/thumb_crt0.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/dw3000_api/thumb_crt0.o: \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/source/thumb_crt0.s
